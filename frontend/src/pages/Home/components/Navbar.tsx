e286a841f1858ad9145cf5b7dbfc50e6d919aca8
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-13 15:11:42 +0530

    feat: add navbar

--- frontend/src/pages/Home/components/Navbar.tsx
+++ frontend/src/pages/Home/components/Navbar.tsx
@@ -1,7 +1,12 @@
 
 function Navbar() {
     return (
-        <div className="bg-primary">Navbar</div>
+        <div className="bg-primary py-3 px-8 flex items-center justify-end font-inter">
+            <p className="text-[16px] font-semibold px-3 text-white">Resume Builder</p>
+            <button className="bg-white px-10 py-2 rounded-full text-center cursor-pointer font-inter text-primary text-sm font-medium">
+                Login/Signup
+            </button>
+        </div>
     )
 }
 
