efa3c444adf04721197f06ce6ff8dbc379749642
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-14 14:52:21 +0530

    feat: Add Hero section

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -11,7 +11,7 @@ interface IProp {
 function Button({ children, classname, variant = "primary", disabled = false }: IProp) {
     return <button
         className={clsx('cursor-pointer transition-all delay-100 inset-0', {
-            'bg-primary px-10 py-3 rounded shadow-md text-[18px] font-semibold text-shadow-md text-neutral-200 hover:bg-secondary hover:text-neutral-100': variant === "primary"
+            'bg-primary px-4 md:px-10 py-3 rounded shadow-md text-[16px] md:text-[18px] font-semibold text-shadow-md text-neutral-200 hover:bg-secondary hover:text-neutral-100': variant === "primary"
         }, classname)}
         disabled={disabled}
     >{children}</button>;
--- frontend/src/pages/Home/sections/HeroSection.tsx
+++ frontend/src/pages/Home/sections/HeroSection.tsx
@@ -1,7 +1,20 @@
-
+import Button from "../../../components/Button"
+import HeroImgSrc from "../../../assets/hero.png"
 function HeroSection() {
     return (
-        <div>HeroSection</div>
+        <div className="m-2 flex flex-col-reverse md:grid md:grid-cols-2 gap-2 md:gap-5 md:px-20 font-rubik">
+            <div className="pt-2 md:pt-44 space-y-4 md:space-y-6 px-5 md:px-0">
+                <p className="text-4xl md:text-6xl text-pretty font-semibold text-primary">Make a resume that stands out</p>
+                <p className="text-[18px] md:text-[20px] font-[500] text-neutral-700/90 text-pretty">As a job seeker, your resume is your ticket to securing employment. A well-crafted resume can be the difference between landing an interview and being overlooked. That’s where a resume maker can come in handy.</p>
+                <Button variant="primary" classname="mt-1 md:mt-4">
+                    Make your resume
+                </Button >
+            </div>
+
+            <div className="flex justify-center items-end">
+                <img src={HeroImgSrc} alt="" className="w-[95%] md:w-4/6 object-cover" />
+            </div>
+        </div>
     )
 }
 
