a74dc287c644dd9886810c30a4b6554603e4bbe7
BeLieVer031 <sandyrajak031@gmail.com>
2025-07-16 10:54:30 +0530

    fix: un-comment

--- frontend/src/pages/Home/page.tsx
+++ frontend/src/pages/Home/page.tsx
@@ -1,7 +1,7 @@
 import CompanyCarousel from "./sections/CompanyCarousel"
 import HeroSection from "./sections/HeroSection"
 import StepSection from "./sections/StepSection"
-// import TemplateCategorySection from "./sections/TemplateCategorySection"
+import TemplateCategorySection from "./sections/TemplateCategorySection"
 import TemplateSection from "./sections/TemplateSection"
 
 function Page() {
@@ -11,7 +11,7 @@ function Page() {
             <CompanyCarousel />
             <TemplateSection />
             <StepSection />
-            {/* <TemplateCategorySection /> */}
+            <TemplateCategorySection />
         </div>
     )
 }
